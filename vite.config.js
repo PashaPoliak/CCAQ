49a40dc483a369638107fde12324beeea20772c5
Pavlo Poliak <pavlo_poliak@epam.com>
2026-06-07 12:44:30 +0300

    regenerate lock file

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/CCAQ/",
+    base: process.env.NODE_ENV === "production" ? "/CCAQ/" : "./",
   test: {
     globals: true,
     environment: "happy-dom",
